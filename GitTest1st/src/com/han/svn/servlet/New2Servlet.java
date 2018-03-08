a452631cfe17bd9a93155f220745deaba073f556
Hanweihu <1752747703@qq.com>
2018-03-08 11:40:27 +0800

    # WARNING: head commit changed in the meantime
    新增一个servlet
    
    
    Signed-off-by: Hanweihu <1752747703@qq.com>

---
+++ GitTest1st/src/com/han/svn/servlet/New2Servlet.java
@@ -0,0 +1,42 @@
+package com.han.svn.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class New2Servlet
+ */
+@WebServlet("/New2Servlet")
+public class New2Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public New2Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 第二次测试
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
